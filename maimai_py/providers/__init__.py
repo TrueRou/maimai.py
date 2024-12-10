df25d22f63fbbae88a8619b2ed7491886bd3f813
Usagi no Niku <chenbohan911@163.com>
2024-12-11 01:37:38 +0800

    fix: add wechat provider to package

--- maimai_py/providers/__init__.py
+++ maimai_py/providers/__init__.py
@@ -2,5 +2,15 @@ from .base import IAliasProvider, IPlayerProvider, ISongProvider, IScoreProvider
 from .divingfish import DivingFishProvider
 from .lxns import LXNSProvider
 from .yuzu import YuzuProvider
+from .wechat import WechatProvider
 
-__all__ = ["IAliasProvider", "IPlayerProvider", "ISongProvider", "IScoreProvider", "DivingFishProvider", "LXNSProvider", "YuzuProvider"]
+__all__ = [
+    "IAliasProvider",
+    "IPlayerProvider",
+    "ISongProvider",
+    "IScoreProvider",
+    "DivingFishProvider",
+    "LXNSProvider",
+    "YuzuProvider",
+    "WechatProvider",
+]
