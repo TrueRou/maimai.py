9810bf0bbfb718be97de7a1ea09f0f56886cb084
ylink <lfs@live.com>
2025-06-24 16:55:52 +0800

    feat: introduce 'MaimaiClientMultithreading' to seperate singleton 'MaimaiClient' from that (#29)
    * feat: introduce 'MaimaiClientMultithreading' to seperate singleton 'MaimaiClient' from that
    
    * improve: singleton warnings and rename tests
    
    * tests: suppress warnings in singleton test
    
    ---------
    
    Co-authored-by: TuRou <turou@email.swu.edu.cn>

---
+++ tests/test_client.py
@@ -0,0 +1,22 @@
+import warnings
+
+import pytest
+
+from maimai_py import MaimaiClient, MaimaiClientMultithreading
+
+
+def test_singleton():
+    m_client1 = MaimaiClient()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        m_client2 = MaimaiClient()
+        assert m_client1 is m_client2
+    m_client_mt1 = MaimaiClientMultithreading(cache_ttl=114)
+    m_client_mt2 = MaimaiClientMultithreading(cache_ttl=514)
+    assert m_client_mt1 is not m_client_mt2
+    assert m_client_mt1._cache_ttl == 114
+    assert m_client_mt2._cache_ttl == 514
+
+
+if __name__ == "__main__":
+    pytest.main(["-q", "-x", "-p no:warnings", "-s", __file__])
