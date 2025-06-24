487ce159ef235a37ea54e62974c0f3e05e067de4
TuRou <turou@email.swu.edu.cn>
2025-06-24 16:54:40 +0800

    tests: suppress warnings in singleton test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+import warnings
+
 import pytest
 
 from maimai_py import MaimaiClient, MaimaiClientMultithreading
@@ -5,8 +7,10 @@ from maimai_py import MaimaiClient, MaimaiClientMultithreading
 
 def test_singleton():
     m_client1 = MaimaiClient()
-    m_client2 = MaimaiClient()
-    assert m_client1 is m_client2
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        m_client2 = MaimaiClient()
+        assert m_client1 is m_client2
     m_client_mt1 = MaimaiClientMultithreading(cache_ttl=114)
     m_client_mt2 = MaimaiClientMultithreading(cache_ttl=514)
     assert m_client_mt1 is not m_client_mt2
